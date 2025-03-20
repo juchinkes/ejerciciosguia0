c284ae4677377b08bad6a313237cc4fe5b4b758a
juchinkes <julic@LAPTOP-VLO9LUGC>
2025-03-20 18:00:28 -0300

    actualización hasta el 24

--- Program.cs
+++ Program.cs
@@ -29,7 +29,13 @@ namespace ejerciciosguia0
             //Ejercicio13();
             // Ejercicio14();
             //Ejercicio16();
-            Ejercicio17();
+            //Ejercicio17();
+            //Ejercicio18();
+            //Ejercicio19();
+            //Ejercicio20();
+            // Ejercicio21();
+            //Ejercicio22();
+            Ejercicio24();
         }
 
         //ejercicio2
@@ -356,7 +362,224 @@ namespace ejerciciosguia0
             Console.WriteLine("El nuúmero que nos dió, redondeado queda así: " + numero1redondeado);
         }
 
-        static void
+        static void Ejercicio18()
+        {
+            double numero1;
+            bool flag = false;
+            
+            do
+            {
+                Console.WriteLine("Ingrese un número");
+                string numero = Console.ReadLine();
+
+                if (!double.TryParse(numero,out numero1))
+                {
+
+
+                    Console.WriteLine("El valor ingresado no es un número");
+                }
+                else
+                {
+                    flag = true;
+                }
+            }while (flag == false);
+
+            int parteentera = (int)numero1;
+            Console.WriteLine("La parte entera del numero ingresado es. " + parteentera);
+
+        }
+
+
+        static void Ejercicio19()
+        {
+            bool Flag = false;
+            int numero1=0;
+            int numero2=0;
+            do
+            {
+                Console.WriteLine("Ingrese el primer número");
+                string input1 = Console.ReadLine();
+                Console.WriteLine("Ingrese el segundo número");
+                string input2 = Console.ReadLine();
+
+                if (!int.TryParse(input1, out numero1) || !int.TryParse(input2, out numero2))
+                {
+                    Console.WriteLine("Alguno de los valores que ingreso no es un número entero");
+
+                }
+                else
+                {
+                    Flag = true;
+
+                }
+
+
+            } while (Flag == false);
+
+
+            if (numero1>numero2)
+            {
+
+                Console.WriteLine("El valor " + numero1 + " es mayor que el valor " + numero2);
+
+            }
+            else if(numero1<numero2)
+            {
+
+                Console.WriteLine("El valor " + numero2 + " es mayor que el valor " + numero1);
+
+            }
+            else
+            {
+                Console.WriteLine("Ambos valores son iguales");
+            }
+
+
+
+        }
+
+        static void Ejercicio20()
+        {
+
+            DateTime fechahoy = DateTime.Now;
+            Console.WriteLine("La fecha del día de hoy es: " + fechahoy);
+            
+        }
+
+        static void Ejercicio21()
+        {
+            DateTime fechahoy = DateTime.Now;
+            string fechaformateada = fechahoy.ToString("dd / MM / yyyy");
+            Console.WriteLine("La fecha del día de hoy es: " + fechaformateada);
+        }
+        
+        static void Ejercicio22()
+        {
+            bool flag = false;
+            DateTime fecha;
+            
+
+            do
+            {
+                Console.WriteLine("Ingrese una fecha");
+                string input = Console.ReadLine();
+
+                if (!DateTime.TryParse(input,out fecha))
+                {
+                    Console.WriteLine("Lo que ingreso no es una fecha");
+                }
+                else
+                {
+                    flag = true;
+                }
+            } while (flag == false);
+
+
+        }
+
+        static void Ejercicio24()
+        {
+            bool flag = false;
+
+            do
+            {
+                Console.WriteLine("Ingrese un día");
+                int dia = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese un mes");
+                int mes = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese un año");
+                int año = Convert.ToInt32(Console.ReadLine());
+
+                if (dia >= 1 && dia <= 30)
+                {
+                    Console.WriteLine("El día está bien");
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine("No puso bien el día");
+
+                }
+                if (mes > 01 && mes <= 12)
+                {
+                    Console.WriteLine("El mes está bien");
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine("No puso bien el mes");
+                }
+                if (año < 2025)
+                {
+
+                    Console.WriteLine("El año está bien");
+                    flag = true;
+                }
+                else
+                {
+
+                    Console.WriteLine("No puso bien el año");
+                }
+            } while (flag == false);
+            
+
+
+        }
+
+        static void Ejercicio25()
+        {
+
+
+        }
+
+        
+        
+        
+        
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
